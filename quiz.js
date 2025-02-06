de40ee2c29fc7aa622a2a73327d0892a14e240f7
glazerarmin <glazer.armin.dominik@students.jedlik.eu>
2025-02-06 10:52:24 +0100

    js elkezdése

---
+++ quiz.js
@@ -0,0 +1,78 @@
+const  questions = [
+    {
+        question: "Hol született Dwayne Johnson?",
+        answers: [
+            { text: "New York", correct: false},
+            { text: "Kalifornia", correct: true},
+            { text: "Miami", correct: false},
+            { text: "London", correct: false},
+        ]
+    },
+    {
+        question: "Hol született Dwayne Johnson?",
+        answers: [
+            { text: "New York", correct: false},
+            { text: "Kalifornia", correct: true},
+            { text: "Miami", correct: false},
+            { text: "London", correct: false},
+        ]
+    },
+    {
+        question: "Hol született Dwayne Johnson?",
+        answers: [
+            { text: "New York", correct: false},
+            { text: "Kalifornia", correct: true},
+            { text: "Miami", correct: false},
+            { text: "London", correct: false},
+        ]
+    },
+    {
+        question: "Hol született Dwayne Johnson?",
+        answers: [
+            { text: "New York", correct: false},
+            { text: "Kalifornia", correct: true},
+            { text: "Miami", correct: false},
+            { text: "London", correct: false},
+        ]
+    },
+    {
+        question: "Hol született Dwayne Johnson?",
+        answers: [
+            { text: "New York", correct: false},
+            { text: "Kalifornia", correct: true},
+            { text: "Miami", correct: false},
+            { text: "London", correct: false},
+        ]
+    },
+    {
+        question: "Hol született Dwayne Johnson?",
+        answers: [
+            { text: "New York", correct: false},
+            { text: "Kalifornia", correct: true},
+            { text: "Miami", correct: false},
+            { text: "London", correct: false},
+        ]
+    },
+    {
+        question: "Hol született Dwayne Johnson?",
+        answers: [
+            { text: "New York", correct: false},
+            { text: "Kalifornia", correct: true},
+            { text: "Miami", correct: false},
+            { text: "London", correct: false},
+        ]
+    },
+    {
+        question: "Hol született Dwayne Johnson?",
+        answers: [
+            { text: "New York", correct: false},
+            { text: "Kalifornia", correct: true},
+            { text: "Miami", correct: false},
+            { text: "London", correct: false},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("answer-buttons");
